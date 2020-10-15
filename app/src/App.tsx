b07c53ac3b05649e3baa2f3f866faa39fde28064
ana.garcia <ana.bya.tvd@gmail.com>
2020-10-15 14:00:51 -0300

    Alterando estado

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,11 +3,13 @@ import { Provider } from "react-redux";
 
 import store from "./store";
 import Catalog from "./componentes/Catalog";
+import Cart from "./componentes/Cart";
 
 function App() {
   return (
     <Provider store={store}>
       <Catalog />
+      <Cart />
     </Provider>
   );
 }
